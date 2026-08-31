b41c6925b77942f85822489128a7bd4fe69beb4f
田昊宇 <tianhaoyu@Mac-mini.local>
2026-08-31 21:18:02 +0800

    Add Twinkle account 14

--- tests/test_workflow_schedule.py
+++ tests/test_workflow_schedule.py
@@ -22,3 +22,4 @@ def test_additional_twinkle_account_secrets_are_wired_into_workflow():
 	assert 'EXTRA_ACCOUNTS_27: ${{ secrets.EXTRA_ACCOUNTS_27 }}' in text
 	assert 'EXTRA_ACCOUNTS_28: ${{ secrets.EXTRA_ACCOUNTS_28 }}' in text
 	assert 'EXTRA_ACCOUNTS_29: ${{ secrets.EXTRA_ACCOUNTS_29 }}' in text
+	assert 'EXTRA_ACCOUNTS_30: ${{ secrets.EXTRA_ACCOUNTS_30 }}' in text
